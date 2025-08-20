029f70c962d47d641616e63689b6477722f9f2cf
Melvin Reyes <olva.reyes@gmail.com>
2025-08-20 15:45:03 -0600

    Set up first node project

---
+++ 02-bases/src/js-foundation/01-template.js
@@ -0,0 +1,10 @@
+const emailTemplate = `
+    <div>
+        <h1>Hola {{name}}</h1>
+        <p>Este es un correo de prueba</p>
+        <p>Saludos</p>
+    </div>
+`;
+
+module.exports = { emailTemplate };
+
