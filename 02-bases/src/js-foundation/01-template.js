2d7fee76ae81b2d5c2e69ad7e6989963d7d656a3
Melvin Reyes <olva.reyes@gmail.com>
2025-08-21 15:27:25 -0600

    destructuring example

--- 02-bases/src/js-foundation/01-template.js
+++ 02-bases/src/js-foundation/01-template.js
@@ -1,7 +1,7 @@
 const emailTemplate = `
     <div>
         <h1>Hola {{name}}</h1>
-        <p>Este es un correo de prueba</p>
+        <p>Este es un correo de prueba!!!</p>
         <p>Saludos</p>
     </div>
 `;
---
+++ 02-bases/src/js-foundation/02-destructuring.js
@@ -0,0 +1,7 @@
+const { SHELL } = process.env;
+
+const characters = ["Flash", "Superman", "Green Lantern", "Batman"];
+
+const [, , , batman] = characters;
+
+console.log(batman); // Batman
